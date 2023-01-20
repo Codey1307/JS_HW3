3f948cdf00d27c27c9dfa3750688a1c7a521304f
Codey <1307>
2023-01-20 19:07:55 +0200

    hw3

---
+++ main.js
@@ -0,0 +1,12 @@
+
+number = +prompt("Введіть число від 0 до 100")
+for (let i = 1; i <= number ; i++) {
+    if(i %5 ===0){
+    console.log(i);
+    }
+}
+if (number < 5){
+    console.log("sorry no numbers");
+}
+
+
